51ad17265e6b2f76e5b4b310bd98db42d2dc62bd
Leandro Ciccarone <lciccarone@vuce.gob.ar>
2021-01-12 23:35:34 -0300

    validators email form

---
+++ evento-virtual/src/app/home/components/formulario/formulario.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {ModalComponent} from './modal/modal.component';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent implements OnInit {
+  form: FormGroup;
+  paises: any[] = ['Argentina', 'Peru', 'Chile'];
+
+  constructor(private modalService: NgbModal) {
+  }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  open() {
+    this.modalService.open(ModalComponent, {centered: true});
+  }
+
+  initForm() {
+    this.form = new FormGroup({
+      nombre: new FormControl(null, [Validators.required]),
+      apellido: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      pais: new FormControl(null, [Validators.required]),
+      telefono: new FormControl(null, [Validators.required]),
+      puesto: new FormControl(null, [Validators.required]),
+    });
+  }
+
+}
